e8f6b01c1e0b1a939b6cc0c481bae92f97fa20c0
Juan Mora Romero <info@juanmoraromero.com>
2021-01-16 21:06:01 +0100

    project over

--- app/js/script.js
+++
@@ -1,6 +0,0 @@
-const testLocal = "Here's a local script file!";
-console.log(testLocal);
-
-const camelCase = require('camelcase');
-const testVar = "testing camelcase in js";
-console.log(camelCase(testVar));
